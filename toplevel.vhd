fca071cec0ecc8b9e5d8e4a1c934382f504e8e22
MATRIX7878 <90746490+MATRIX7878@users.noreply.github.com>
2024-06-06 18:22:27 -0700

    Update toplevel.vhd

--- toplevel.vhd
+++ toplevel.vhd
@@ -14,6 +14,8 @@ END ENTITY;
 
 ARCHITECTURE behavior OF toplevel IS
 
+SIGNAL start : STD_LOGIC := 1;
+
 SIGNAL rd, wr, refresh, clk_x4, lock, clk : STD_LOGIC;
 SIGNAL din : STD_LOGIC_VECTOR (15 DOWNTO 0);
 SIGNAL dOUT : STD_LOGIC_VECTOR (15 DOWNTO 0);
@@ -26,7 +28,7 @@ SIGNAL wstep : STD_LOGIC_VECTOR (7 DOWNTO 0);
 SIGNAL debug : STD_LOGIC_VECTOR (63 DOWNTO 0);
 
 CONSTANT freq : INTEGER := 99800000;
-CONSTANT total_size : INTEGER := 8 * 1024 * 1024;
+CONSTANT TOTAL_SIZE : INTEGER := 8 * 1024 * 1024;
 
 CONSTANT INIT : INTEGER := 0;
 CONSTANT PRINT : INTEGER := 1;
@@ -41,7 +43,36 @@ CONSTANT VERIFY : INTEGER := 9;
 CONSTANT WIPE : INTEGER := 10;
 CONSTANT FINISH : INTEGER := 11;
 
-SIGNAL start_addr : STD_LOGIC_VECTOR (25 DOWNTO 0) := (OTHERS => '0');
+SIGNAL state, endState, workCount, latWr1, latWr2, latR : STD_LOGIC_VECTOR (7 DOWNTO 0);
+
+SIGNAL error, refreshNeeded, refreshExec : STD_LOGIC;
+
+SIGNAL refreshTime : STD_LOGIC_VECTOR (11 DOWNTO 0);
+CONSTANT REFRESHCOUNT : REAL := freq / 1000 / 1000 * 7813 / 1000;
+
+SIGNAL refreshCycle : STD_LOGIC;
+SIGNAL refreshCnt : STD_LOGIC_VECTOR (23 DOWNTO 0);
+SIGNAL refreshAddr : STD_LOGIC_VECTOR (24 DOWNTO 0);
+
+TYPE debugdq IS ARRAY (64 DOWNTO 0) OF STD_LOGIC_VECTOR (15 DOWNTO 0);
+SIGNAL debugdqbuf : debugdq;
+
+SIGNAL debugCycle : STD_LOGIC_VECTOR (3 DOWNTO 0);
+SIGNAL tickCount :STD_LOGIC_VECTOR (19 DOWNTO 0);
+SIGNAL tick : STD_LOGIC;
+SIGNAL rsltPrint : STD_LOGIC;
+SIGNAL expect, actual : STD_LOGIC_VECTOR (15 DOWNTO 0);
+SIGNAL actual128 : STD_LOGIC_VECTOR (127 DOWNTO 0);
+SIGNAL addrRead : STD_LOGIC_VECTOR (25 DOWNTO 0);
+SIGNAL wlevelFeed : STD_LOGIC;
+SIGNAL wlevelDone : STD_LOGIC := 0;
+SIGNAL rlevelDone := 0;
+SIGNAL readCnt : STD_LOGIC_VECTOR (7 DOWNTO 0);
+
+TYPE BYTE IS ARRAY (7 DOWNTO 0) OF BIT;
+TYPE ADDR IS ARRAY (25 DOWNTO 0) OF BIT;
+
+SIGNAL START_ADDR : STD_LOGIC_VECTOR (25 DOWNTO 0) := (OTHERS => '0');
 
 COMPONENT Gowin_rPLL
     PORT (
@@ -70,9 +101,9 @@ COMPONENT DDR3 IS
          rclksel : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
          wstep : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
          DDR3_dq : INOUT STD_LOGIC_VECTOR (15 DOWNTO 0);
-         dOUT : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
+         dout : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
          debug : OUT STD_LOGIC_VECTOR (63 DOWNTO 0);
-         dOUT128 : OUT STD_LOGIC_VECTOR (127 DOWNTO 0);
+         dout128 : OUT STD_LOGIC_VECTOR (127 DOWNTO 0);
          DDR3_a : OUT STD_LOGIC_VECTOR (ROW_WIDTH - 1 DOWNTO 0);
          DDR3_ba : OUT STD_LOGIC_VECTOR (BANK_WIDTH - 1 DOWNTO 0));
 END COMPONENT;
@@ -101,6 +132,191 @@ BEGIN
                   rclksel => rclksel, wstep => wstep, DDR3_dq => DDR3_dq, dOUT => dOUT,
                   debug => debug, dOUT128 => dOUT128, DDR3_a => DDR3_a, DDR3_ba => DDR3_ba);
 
+    PROCESS(ALL)
+        BEGIN
+        IF RISING_EDGE(clk) THEN
+            IF state THEN
+                refreshTime <= (REFRESHCOUNT * 2 - 2) WHEN refreshTime = (REFRESHCOUNT * 2 - 2) ELSE refreshTime + 1;
+                IF refreshTime = REFRESHCOUNT THEN
+                    refreshNeeded <= '1';
+                END IF;
+                IF refreshExec THEN
+                    refreshTime <= refreshTime - REFRESHCOUNT;
+                    refreshNeeded <= '0';
+                END IF;
+                IF NOT reset THEN
+                    refreshTime <= '0';
+                    refreshNeeded <= '0';
+                END IF;
+            END IF;
+        END IF;
+    END PROCESS;
 
+    PROCESS(ALL)
+        BEGIN
+        IF RISING_EDGE(clk) THEN
+            wr <= '0';
+            rd <= '0';
+            refresh <= '0';
+            refreshExec <= '0';
+            workCount <= workCount + 1;
+            tickCount <= "0" WHEN tickCount = 0 ELSE tickCount - 1;
+            tick <= tickCount = 1;
 
+            CASE state IS
+                WHEN INIT => IF (lock AND reset AND NOT busy AND start) THEN
+                        tickCount <= d"100000";
+                        state <= PRINT;
+                    END IF;
+                WHEN PRINT => IF tick THEN
+                        tickCount <= d"100000";
+                        workCount <= '0';
+                        addr = START_ADDR;
+                        state <= WRITE1;    
+                    END IF;
+                WHEN WRITE1 => IF tick THEN
+                        wr <= '1';
+                        addr <= x"0000";
+                        din <= x"1122";
+                        workCount <= '0';
+                        tickCount <= d"100000";
+                        state <= WRITE2;
+                        END IF;
+                WHEN WRITE2 => IF tick THEN
+                        wr <= '1';
+                        addr <= x"0001";
+                        din <= x"3344";
+                        workCount <= '0';
+                        tickCount <= d"100000";
+                        state <= WRITE3;
+                        END IF;
+                WHEN WRITE3 => IF tick THEN
+                        IF busy THEN
+                            error <= '1';
+                        END IF;
+                        latWr1 <= workCount(7 DOWNTO 0);
+                        wr <= '1';
+                        addr <= x"0002";
+                        din <= x"5566";
+                        workCount <= '0';
+                        debugCycle <= '0';
+                        tickCount <= d"100000";
+                        state <= READSTART;
+                        END IF;
+                WHEN READSTART => IF tick THEN
+                        addr(15 DOWNTO 0) <= x"0000";
+                        tickCount <= d"100000";
+                        state <= READ;
+                        END IF;
+                WHEN READ => rsltPrint <= '0';
+                        IF tick THEN
+                            IF addr(15 DOWNTO 0) = x"0003" THEN
+                                tickCount <= d"200000";
+                                state <= READDONE;
+                            ELSE
+                                rd <= '1';
+                                tickCount <= x"200000";
+                            END IF;
+                        ELSIF data_ready THEN
+                            actual <= dout;
+                            actual128 <= dout128;
+                            addrRead <= addr;
+                            rsltPrint <= '1';
+                            addr(15 DOWNTO 0) <= addr(15 DOWNTO 0) + 1;
+                        END IF;
+                WHEN READDONE => addr <= START_ADDR;
+                        workCount <= '0';
+                        state <= WIPE;
+                WHEN WIPE => IF (addr = (START_ADDR + TOTAL_SIZE)) THEN
+                            workCount <= '0';
+                            addr <= START_ADDR;
+                            state <= WRITEBLOCK;
+                        ELSE
+                            IF workCount = 0 THEN
+                                IF NOT refreshNeeded THEN
+                                    wr <= '1';
+                                    din <= '0';
+                                    refreshCycle <= '0';
+                                ELSE
+                                    refresh <= '1';
+                                    refreshCnt <= refreshCnt + 1;
+                                    refreshAddr <= addr;
+                                    refreshExec <= '1';
+                                    refreshCycle <= '1';
+                                END IF;
+                            ELSIF (NOT wr AND NOT refresh AND NOT busy) THEN
+                                workCount <= '0';
+                                IF NOT refreshCycle THEN
+                                    addr <= addr + 1;
+                                END IF;
+                            END IF;
+                        END IF;
+                WHEN WRITEBLOCK => IF (addr = (START_ADDR + TOTAL_SIZE)) THEN
+                            addr <= START_ADDR;
+                            workCount <= '0';
+                            state <= VERIFY;
+                        ELSE
+                            IF workCount = 0 THEN
+                                IF NOT refreshNeeded THEN
+                                    wr <= '1';
+                                    din <= addr(15 DOWNTO 0) XOR ("000000" & addr(25 DOWNTO 16)) XOR x"59";
+                                    refreshCycle <= '0';
+                                ELSE
+                                    refresh <= '1';
+                                    refreshCnt <= refreshCnt + 1;
+                                    refreshAddr <= addr;
+                                    refreshExec <= '1';
+                                    refreshCycle <= '1';
+                                END IF;
+                            ELSIF (NOT wr AND NOT refresh AND NOT busy) THEN
+                                workCount <= '0';
+                                IF NOT refreshCycle THEN
+                                    addr <= addr + 1;
+                                END IF;
+                            END IF;
+                        END IF;
+                WHEN VERIFY => IF (addr = (START_ADDR + TOTAL_SIZE)) THEN
+                            endState <= state;
+                            state <= VERIFY;
+                        ELSE
+                            IF workCount = 0 THEN
+                                IF NOT refreshNeeded THEN
+                                    rd <= '1';
+                                    refreshCycle <= '0';
+                                ELSE
+                                    refresh <= '1';
+                                    refreshCnt <= refreshCnt + 1;
+                                    refreshAddr <= addr;
+                                    refreshExec <= '1';
+                                    refreshCycle <= '1';
+                                END IF;
+                            ELSIF (data_ready) THEN
+                                expect <= addr(15 DOWNTO 0) XOR ("000000" & addr(25 DOWNTO 16)) XOR x"59";
+                                actual <= dout;
+                                actual128 <= dout128;
+                                IF (dout(7 DOWNTO 0) /= (addr XOR ("000000" & addr(25 DOWNTO 16)) XOR d"59") THEN
+                                    error <= '1';
+                                    endState <= state;
+                                    state <= FINISH;
+                                END IF;
+                            ELSIF (NOT wr AND NOT refresh AND NOT busy) THEN
+                                workCount <= '0';
+                                IF NOT refreshCycle THEN
+                                    addr <= addr + 1;
+                                END IF;
+                            END IF;
+                        END IF;
+            END CASE;
+            IF NOT reset THEN
+                error <= '0';
+                tick <= '0';
+                tickCount <= d"100000";
+                latWr1 <= '0';
+                latWr2 <= '0';
+                latR <= '0';
+                refreshCnt <= '0';
+                state <= INIT;
+            END IF;
+        END IF;
+    END PROCESS;
 END ARCHITECTURE;
